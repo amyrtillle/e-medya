7ba131df14a4fa641d32aec15ea3bfd8c829aaee
Emy <emy@easy-mountain.com>
2023-09-07 11:25:07 +0200

    feat: navBar

--- packages/components/src/molecules/EmNavBar/EmNavBar.vue
+++ packages/components/src/molecules/EmNavBar/EmNavBar.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
 import { EmButton, EmTextContent } from '../../components'
-import { variant } from '../../types'
-
-withDefaults(defineProps<{ variant: variant }>(), {
-  variant: 'primary',
-})
 </script>
 
 <template>
-  <nav class="nav" :class="{ [`variant-${variant}`]: true }">
+  <nav class="nav">
     <EmTextContent tag="a" size="m" href="./services" text="Services" />
     <EmTextContent tag="a" size="m" href="./productions" text="Productions" />
     <EmTextContent tag="a" size="m" href="./about" text="A propos" />
@@ -19,13 +14,38 @@ withDefaults(defineProps<{ variant: variant }>(), {
 
 <style scoped>
 nav{
+  background-color: var(--em-color-purple-mountain-majesty);
   display: flex;
-  gap: 40px;
-  align-items: center;
+  align-items: end;
+  flex-direction: column;
+  width: fit-content;
+  height: fit-content;
+  padding: 50px 10px 50px 50px;
+  gap: 30px;
+  border-radius: 15px 0px 0px 15px;
+}
+
+nav .em-button:first-of-type{
+  margin-top: 80px;
 }
 
 a{
   text-decoration: none;
   font-weight: var(--em-typography-font-weight-semibold);
+}
+
+@media screen and (min-width: 768px) {
+nav{
+  background-color: transparent;
+  gap: 40px;
+  align-items: center;
+  width: 100%;
+  border-radius: 0;
+  flex-direction: row;
+  padding: 0;
+}
+nav .em-button:first-of-type{
+  margin: 0;
+}
 }
 </style>
