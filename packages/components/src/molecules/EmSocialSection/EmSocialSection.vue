b85cab4687b869cbe77688cf307729dc48cc74d2
Emy <emy@easy-mountain.com>
2023-09-21 10:03:49 +0200

    fix: footer style

--- packages/components/src/molecules/EmSocialSection/EmSocialSection.vue
+++ packages/components/src/molecules/EmSocialSection/EmSocialSection.vue
@@ -13,8 +13,8 @@ const props = {
 <template>
   <div class="social-section">
     <EmHeadLine text="RESEAUX" variant="secondary" />
-    <div v-for="src in props.list" class="social-icons">
-      <a :href="`http://${src}.com/e-medya`">
+    <div class="social-icons-container">
+      <a v-for="src in props.list" :href="`http://${src}.com/e-medya`" class="social-icons">
         <EmIcon
           :src="src"
           :class="src"
@@ -28,10 +28,31 @@ const props = {
 <style scoped>
 .social-section{
   display: flex;
+  flex-direction: column;
+}
+
+.social-icons-container{
+  gap: var(--em-spacing-s);
+  display: flex;
+  flex-direction: row;
+width: fit-content;
+margin-top: var(--em-spacing-s);
+}
+
+@media screen and (min-width: 768px) {
+.social-section{
   align-items: center;
+  flex-direction: row;
 }
 
 .social-icons{
   margin-left: var(--em-spacing-m)
+}
+
+.social-icons-container{
+  flex-direction: row;
+  gap: 0;
+  margin: 0;
+}
 }
 </style>
