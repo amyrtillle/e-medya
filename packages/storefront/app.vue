280c436f05da2423f5f8cd4953b2e34edb6cca98
amyrtillle <pommefrite2003@gmail.com>
2024-05-24 02:22:11 +0200

    chore: add gsc verification

--- packages/storefront/app.vue
+++ packages/storefront/app.vue
@@ -1,5 +1,8 @@
 <template>
   <NuxtLayout>
+    <head>
+      <meta name="google-site-verification" content="HQgvZ8ajvn02_K1ZpAAtwh9NmkFoor3bPgw6SwEmHAM">
+    </head>
     <NuxtPage />
   </NuxtLayout>
 </template>
